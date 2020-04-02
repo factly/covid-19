d8d7865d6bef13a7fbce44c69c883c65d5398b0e
Mahipat <mj@Mojaave.com>
2020-04-02 09:53:25 +0530

    whitelist selector in purge css

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,17 +99,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
@@ -128,6 +117,17 @@ module.exports = {
         theme_color: themeColor,
         display: 'minimal-ui',
         icon: favicon
+      }
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        whitelist: ['blockquote'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     },
     'gatsby-plugin-offline',
