2313717e91ce0643a9732bf9b010245e49ae7ac8
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-08-14 02:59:01 +0530

    update siteurl

--- data/site-config.js
+++ data/site-config.js
@@ -3,7 +3,7 @@ module.exports = {
   shortName: 'Factly',
   title: 'FACTLY - Making Public Data Meaningful',
   logo: `${__dirname}/../src/static/images/logo/factly-logo.png`,
-  siteUrl: 'https://factly.in/',
+  siteUrl: 'https://covid19.factly.in/',
   description:
     'FACTLY is one of the well known Data Journalism/Public Information portals in India. Each news story on FACTLY is backed by factual evidence/data from official sources that is either available in the public domain or that is collated/gathered/collected using tools such as the Right to Information (RTI).',
   type: 'website',
