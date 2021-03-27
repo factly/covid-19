df4427246237a4e16ccd4ab32aa12cc0efa32960
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-03-27 06:57:25 +0530

    add plausible to telugu version

---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  setPostBodyComponents([
+    <script key={1} async defer data-domain="covid19-telugu.factly.in" src="https://plausible.io/js/plausible.js"></script>
+  ]);
+};
